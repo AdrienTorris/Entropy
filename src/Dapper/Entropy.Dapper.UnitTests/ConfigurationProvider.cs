670d1da83ed7969b3a402268a77b8366b2430c82
Adrien TORRIS <adrientorris@gmail.com>
2017-04-18 16:00:20 +0200

    Dapper playground

---
+++ src/Dapper/Entropy.Dapper.UnitTests/ConfigurationProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entropy.Dapper.UnitTests
+{
+    internal static class ConfigurationProvider
+    {
+        internal const string _connection_string = @"";
+    }
+}
---
+++ src/Dapper/Entropy.Dapper.UnitTests/QueryingTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dapper;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Entropy.Dapper.UnitTests
+{
+    /// <summary>
+    /// Simple test cases about querying SQL Server data with Dapper
+    /// </summary>
+    /// <see cref="https://stackexchange.github.io/Dapper/"/>
+    [TestClass]
+    public class QueryingTests
+    {
+        [TestMethod]
+        public void TestGetItem()
+        {
+            IEnumerable<Class1> ret;
+
+            try
+            {
+                using (DbConnection con = new SqlConnection(ConfigurationProvider._connection_string))
+                {
+                    ret = con.Query<Class1>("select * from Anomalie WHERE Id = @Id", new { Id = 1 });
+                }
+
+                Assert.IsNotNull(ret);
+                Assert.AreEqual<int>(1, ret.Count());
+                Assert.AreEqual<int>(1, ret.First().Id);
+            }
+            catch (Exception)
+            {
+                Assert.IsTrue(false);
+            }
+            finally
+            {
+                ret = null;
+            }
+        }
+
+        [TestMethod]
+        public void TestListItems()
+        {
+            IEnumerable<Class1> ret;
+
+            try
+            {
+                using (DbConnection con = new SqlConnection(ConfigurationProvider._connection_string))
+                {
+                    ret = con.Query<Class1>("select * from Anomalie", new { Id = 1 });
+                }
+
+                Assert.IsNotNull(ret);
+                Assert.AreNotEqual<int>(0, ret.Count());
+            }
+            catch (Exception)
+            {
+                Assert.IsTrue(false);
+            }
+        }
+
+        [TestMethod]
+        public void TestGetItemWithTransaction()
+        {
+            IEnumerable<Class1> ret;
+
+            try
+            {
+                using (DbConnection con = new SqlConnection(ConfigurationProvider._connection_string))
+                {
+                    if (con.State != System.Data.ConnectionState.Open)
+                        con.Open();
+
+                    using (DbTransaction trans = con.BeginTransaction())
+                    {
+                        ret = con.Query<Class1>("select * from Anomalie WHERE Id = @Id", new { Id = 1 }, transaction: trans);
+                    }
+                }
+
+                Assert.IsNotNull(ret);
+                Assert.AreEqual<int>(1, ret.Count());
+                Assert.AreEqual<int>(1, ret.First().Id);
+            }
+            catch (Exception)
+            {
+                Assert.IsTrue(false);
+            }
+            finally
+            {
+                ret = null;
+            }
+        }
+
+        public class Class1
+        {
+            public int Id { get; set; }
+            public string Code { get; set; }
+            public string Label { get; set; }
+        }
+    }
+}
