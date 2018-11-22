30d1b3f5839f02dd4ee547771257ed7582d62f70
Adrien Torris <adrientorris@gmail.com>
2018-11-22 19:59:17 +0100

    Razor Class Library Sample
    Add some tests about the Razor Class Libraries.

---
+++ src/RazorClassLibrarySample/Module1/UiConfigureOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Module1
+{
+    using Microsoft.AspNetCore.Hosting;
+    using ModularizationCore.Mvc;
+
+    public sealed class UiConfigureOptions : BaseModuleUiConfigureOptions
+    {
+        public UiConfigureOptions(IHostingEnvironment environment)
+            : base(environment)
+        {
+        }
+    }
+}
---
+++ src/RazorClassLibrarySample/Module2/Areas/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module2.Areas.Feature2.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public IActionResult Index2()
+        {
+            return Content("lkdlkkdslkds");
+        }
+    }
+}
