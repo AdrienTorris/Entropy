e6b396c5ad10e1fbd8c41d2557ffa0648b05cb8a
Adrien Torris <adrientorris@gmail.com>
2022-07-03 00:16:39 +0200

    Added a benchmark to hashids lab

---
+++ src/HashidsLab/Hashids.ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using HashidsNet;
+
+var benchmarkConfig = new ManualConfig()
+                    .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+                    .AddValidator(JitOptimizationsValidator.DontFailOnError)
+                    .AddLogger(ConsoleLogger.Default)
+                    .AddColumnProvider(DefaultColumnProviders.Instance);
+BenchmarkRunner.Run<HashidsBenchmark>(benchmarkConfig);
+
+[MemoryDiagnoser(false)]
+public class HashidsBenchmark
+{
+    private static readonly Hashids hashids = new ("testlab", 8);
+
+    [Benchmark]
+    public int IntFromHash()
+    {
+        return hashids.Decode("rBaLjAgw")[0];
+    }
+
+    [Benchmark]
+    public string HashFromInt()
+    {
+        return hashids.Encode(1);
+    }
+}
